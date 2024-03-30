08e671a503711161afd10af7ffb0d879267839ee
slowjams <lxdthriller@gmail.com>
2024-03-31 00:45:26 +1100

    more

--- zProjects/frontend/src/App.js
+++ zProjects/frontend/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Bye there!
+          Bye Bye Bye!
         </p>
         <a
           className="App-link"
